24ed1ea1850986e8fd80eb97a8a59729b5e92f2e
Kuba Karasek <jakkarasek@gmail.com>
2025-04-26 05:04:06 +0200

    fix: update counter route name in navbar

--- components/app/navbar.vue
+++ components/app/navbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav class="flex items-center h-12 px-4 shadow-md">
 		<ul class="flex gap-4">
-			<li><NuxtLink to="/">Counter</NuxtLink></li>
+			<li><NuxtLink :to="{ name: 'counter' }">Counter</NuxtLink></li>
 			<li><NuxtLink :to="{ name: 'input' }">Input</NuxtLink></li>
 			<li><NuxtLink :to="{ name: 'data-fetching' }">Data Fetching</NuxtLink></li>
 		</ul>
